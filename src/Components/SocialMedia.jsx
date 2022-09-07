fc225a34d5b46eb78d785e2fd9403638fd73a97d
nassim-sadi <nacimbreeze@gmail.com>
2022-09-07 20:29:11 +0100

    added socialMedia links

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className='app__social'>
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
+    <a href='https://github.com/Nassim-sadi' target='_blank' rel='noreferrer'>
+      <div>
+        <FaGithub />
+      </div>
+    </a>
+    <a href='https://twitter.com/Nassimsadi4' target='_blank' rel='noreferrer'>
+      <div>
+        <FaTwitter />
+      </div>
+    </a>
+    <a href='https://www.linkedin.com/in/nassim-sadi-22587b156/' target='_blank' rel='noreferrer'>
+      <div>
+        <FaLinkedin />
+      </div>
+    </a>
   </div>
 );
 
