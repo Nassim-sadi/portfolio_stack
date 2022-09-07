331107f57ec7a8551312da917788ebb6c0e23b09
nassim-sadi <nacimbreeze@gmail.com>
2022-09-07 20:49:45 +0100

    added animation for social icons

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 const SocialMedia = () => (
   <div className='app__social'>
-    <a href='https://github.com/Nassim-sadi' target='_blank' rel='noreferrer'>
-      <div>
-        <FaGithub />
-      </div>
-    </a>
-    <a href='https://twitter.com/Nassimsadi4' target='_blank' rel='noreferrer'>
-      <div>
-        <FaTwitter />
-      </div>
-    </a>
-    <a href='https://www.linkedin.com/in/nassim-sadi-22587b156/' target='_blank' rel='noreferrer'>
-      <div>
-        <FaLinkedin />
-      </div>
-    </a>
+    <motion.div whileHover={{ rotate: 360, scale: 1.2 }} transition={{ duration: 0.1 }}>
+      <a href='https://github.com/Nassim-sadi' target='_blank' rel='noreferrer'>
+        <div>
+          <FaGithub />
+        </div>
+      </a>
+    </motion.div>
+    <motion.div whileHover={{ rotate: 360, scale: 1.2 }} transition={{ duration: 0.1 }}>
+      <a href='https://twitter.com/Nassimsadi4' target='_blank' rel='noreferrer'>
+        <div>
+          <FaTwitter />
+        </div>
+      </a>
+    </motion.div>
+    <motion.div whileHover={{ rotate: 360, scale: 1.2 }} transition={{ duration: 0.1 }}>
+      <a href='https://www.linkedin.com/in/nassim-sadi-22587b156/' target='_blank' rel='noreferrer'>
+        <div>
+          <FaLinkedin />
+        </div>
+      </a>
+    </motion.div>
   </div>
 );
 
