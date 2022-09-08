dc4ac29dcce014edf38e06ed0faa77497152dd94
nassim-sadi <nacimbreeze@gmail.com>
2022-09-08 19:31:23 +0100

    glass effect on about items

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -21,21 +21,23 @@ const About = () => {
       </h2>
       <div className='app__profiles'>
         {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className='app__profile-item'
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className='p-text' style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
+          <div className='app__profile-parent'>
+            <div className='app__profile-background'></div>
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className='app__profile-item'
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className='bold-text' style={{ marginTop: 20 }}>
+                {about.title}
+              </h2>
+              <p className='p-text' style={{ marginTop: 10 }}>
+                {about.description}
+              </p>
+            </motion.div>
+          </div>
         ))}
       </div>
     </>
